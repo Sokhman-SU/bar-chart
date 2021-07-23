e0fd9778d1b008a11dce7bbb78f90ec6798dce67
Samuel Okhman <samokhman@samuels-mbp-2.myfiosgateway.com>
2021-07-23 09:19:36 -0400

    Initial App Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,195 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Select from "react-select";
+import { Bar } from "react-chartjs-2";
+import program from "./programData.json";
+import _ from 'lodash';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "./App.css";
 
-function App() {
+const options = [
+  { label: "Viewers By Network", value: "network" },
+  { label: "Viewers By genre", value: "genre" },
+  { label: "Viewers By Hometown", value: "hometown" }
+];
+
+
+const networkarray = _(program)
+  .groupBy('network')
+  .map((network, id) => ({
+    network: id,
+    viewers: _.sumBy(network, 'viewers'),
+  }))
+  .value()
+
+  
+const cbsarray = networkarray[0];
+const cbsviewers = cbsarray.viewers;
+
+const abcarray = networkarray[1];
+const abcviewers = abcarray.viewers;
+
+const syfyarray = networkarray[2];
+const syfyviewers = syfyarray.viewers;
+
+const genrearray = _(program)
+  .groupBy('genre')
+  .map((genre, id) => ({
+    genre: id,
+    viewers: _.sumBy(genre, 'viewers'),
+  }))
+  .value()
+
+  console.log(genrearray);
+
+const sportsarray = genrearray[0];
+const sportsviewers = sportsarray.viewers;
+
+const mysteryarray = genrearray[1];
+const mysteryviewers = mysteryarray.viewers;
+
+const sfarray = genrearray[2];
+const sfviewers = sfarray.viewers;
+
+const hometownarray = _(program)
+  .groupBy('hometown')
+  .map((hometown, id) => ({
+    hometown: id,
+    viewers: _.sumBy(hometown, 'viewers'),
+  }))
+  .value()
+
+  console.log(hometownarray);
+
+const pittarray = hometownarray[0];
+const pittviewers = pittarray.viewers;
+console.log(pittviewers);
+
+const newarray = hometownarray[1];
+const newviewers = newarray.viewers;
+
+const bosarray = hometownarray[2];
+const bosviewers = bosarray.viewers;
+
+const clevarray = hometownarray[3];
+const clevviewers = clevarray.viewers;
+
+function requestApi(network) {
+  let result;
+  switch (network) {
+    case "network":
+      result = {
+        labels: ['CBS', 'ABC', 'SyFy'],
+        datasets: [
+          {
+            label: "Network Viewship",
+            data: [cbsviewers, abcviewers, syfyviewers],
+            backgroundColor: 'red',
+            borderWidth: 1
+          }
+        ]
+      };
+      break;
+
+    case "genre":
+      result = {
+        labels: ['Sports', 'Mystery', 'Science Fiction'],
+        datasets: [
+          {
+            label: "Genre Viewship",
+            data: [sportsviewers, mysteryviewers, sfviewers],
+            backgroundColor: 'blue',
+            borderWidth: 1
+          }
+        ]
+      };
+      break;
+
+      case "hometown":
+        result = {
+          labels: ['Pittsburgh', 'New York', 'Boston', 'Cleveland'],
+          datasets: [
+            {
+              label: "Hometown Viewship",
+              data: [pittviewers, newviewers, bosviewers, clevviewers],
+              backgroundColor: 'green',
+              borderWidth: 1
+            }
+          ]
+        };
+        break;
+
+    default:
+      break;
+  }
+  return Promise.resolve(result);
+}
+
+function getDataFromNetwork(network) {
+  return requestApi(network);
+}
+
+
+
+const defaultNetwork = options[0];
+const defaultData = {};
+
+export default function App() {
+  const [network, setNetwork] = React.useState(defaultNetwork.value);
+  const [chartData, setChartData] = useState(defaultData);
+
+  const handleChange = (value) => {
+    const network = value.value;
+    setNetwork(network);
+  };
+
+  React.useEffect(() => {
+    getDataFromNetwork(network).then((chartData) => {
+      setChartData(chartData);
+    });
+  }, [network]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-one">
+      <h1>Tv Viewership</h1>
+     
+      <Tabs className="tabs">
+        <TabList>
+          <Tab>Filter By: Viewship</Tab>
+          <Tab>Filter By: Location</Tab>
+        </TabList>
+
+        <TabPanel>
+        <span className="dropdown-select">
+        <Select
+          options={options}
+          defaultValue={defaultNetwork}
+          onChange={handleChange}
+        />
+      </span>
+          <Bar
+          data={chartData}
+          options={{
+            responsive: true,
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }
+              ]
+            },
+
+          }}
+          height={125}
+        />
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+     
     </div>
   );
 }
 
-export default App;
